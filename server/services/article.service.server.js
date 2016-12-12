8b9538635ed19bc5a0592d7cd6d21fdfb1638c2f
Mayank Ranjan Dayal <mayank.dyl@gmail.com>
2016-12-12 01:45:52 -0500

    article&comment service server

---
+++ server/services/article.service.server.js
@@ -0,0 +1,112 @@
+/**
+ * Created by mayank on 12/10/16.
+ */
+module.exports = function (app, model) {
+
+    var ArticleModel = model.articleModel;
+
+    console.log("inside article server service");
+
+    app.post('/api/article', createArticle);
+    app.post('/api/findAllarticles', findAllArticles);
+    app.get('/api/article/:aid', findArticleById);
+    app.put('/api/article/:aid', updateArticleById);
+    app.delete('/api/article/:aid', deleteArticleById);
+
+    function deleteArticleById(req, res){
+        var artId = req.params.aid;
+        var articles;
+        ArticleModel.deleteArticleById(artId)
+            .then(
+                function(doc){
+                    articles = doc;
+                    res.json(articles);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function updateArticleById(req, res){
+        var artId = req.params.aid;
+        var userParams = req.body;
+        var article;
+        console.log("userparams");
+        console.log(userParams);
+        console.log(artId);
+        ArticleModel.updateArticleById(artId, userParams)
+            .then(
+                function(doc){
+                    article = doc;
+                    res.json(article);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function findArticleById(req, res){
+        var artId = req.params.aid;
+        console.log(artId);
+        var article;
+        ArticleModel.findArticleById(artId)
+            .then(
+                function(doc){
+                    user = doc;
+                    console.log("current user");
+                    console.log(article);
+                    res.json(article);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function findAllArticles(req, res){
+        var articles = [];
+        ArticleModel.findAllArticles()
+            .then(
+                function(doc){
+                    articles = doc;
+                    res.json(articles);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function createArticle(req, res){
+        var article = req.body;
+        console.log(article);
+        ArticleModel.createArticle(user)
+            .then(
+                function(doc){
+                    article = doc;
+                    res.json(article);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+
+    function login(req, res){
+        var user = req.user;
+        //UserModel.setUserLoggedIn(user._id);
+        res.json(user);
+    }
+
+    function logout(req, res){
+        req.logout();
+        res.send(200);
+    }
+
+    function loggedIn(req, res){
+        res.send(req.isAuthenticated()? req.user: '0');
+    }
+};
---
+++ server/services/comment.service.server.js
@@ -0,0 +1,112 @@
+/**
+ * Created by mayank on 12/10/16.
+ */
+module.exports = function (app, model) {
+
+    var CommentModel = model.commentModel;
+
+    console.log("inside comment server service");
+
+    app.post('/api/comment', createComment);
+    app.post('/api/findAllComment', findAllComment);
+    app.get('/api/comment/:cid', findCommentById);
+    app.put('/api/comment/:cid', updateCommentById);
+    app.delete('/api/comment/:cid', deleteCommentById);
+
+    function deleteCommentById(req, res){
+        var commentId = req.params.cid;
+        var comments;
+        CommentModel.deleteCommentById(commentId)
+            .then(
+                function(doc){
+                    comments = doc;
+                    res.json(comments);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function updateCommentById(req, res){
+        var commentId = req.params.cid;
+        var commParams = req.body;
+        var comment;
+        console.log("commParams");
+        console.log(commParams);
+        console.log(commentId);
+        CommentModel.updateCommentById(commentId, commParams)
+            .then(
+                function(doc){
+                    comment = doc;
+                    res.json(comment);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function findCommentById(req, res){
+        var commentId = req.params.cid;
+        console.log(commentId);
+        var comment;
+        CommentModel.findCommentById(commentId)
+            .then(
+                function(doc){
+                    comment = doc;
+                    console.log("current comment");
+                    console.log(comment);
+                    res.json(comment);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function findAllComment(req, res){
+        var comments = [];
+        CommentModel.findAllComment()
+            .then(
+                function(doc){
+                    comments = doc;
+                    res.json(comments);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+    function createComment(req, res){
+        var comment = req.body;
+        console.log(comment);
+        CommentModel.createUser(comment)
+            .then(
+                function(doc){
+                    comment = doc;
+                    res.json(comment);
+                },
+                function(err){
+                    res.status(400).send(err);
+                }
+            )
+    }
+
+
+    function login(req, res){
+        var user = req.user;
+        //UserModel.setUserLoggedIn(user._id);
+        res.json(user);
+    }
+
+    function logout(req, res){
+        req.logout();
+        res.send(200);
+    }
+
+    function loggedIn(req, res){
+        res.send(req.isAuthenticated()? req.user: '0');
+    }
+};
